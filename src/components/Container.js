ce20ca7faa4c99f9bb010a8f792e4bcb2a55da8c
wnpraf <rapribu@gmail.com>
2020-05-26 22:12:01 +0200

    Add Container component

---
+++ src/components/Container.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.section`
+  margin: 0 auto;
+  width: 100%;
+  @media (min-width: 640px) {
+    max-width: 640px;
+  }
+  @media (min-width: 768px) {
+    max-width: 768px;
+  }
+  @media (min-width: 1024px) {
+    max-width: 1024px;
+  }
+  @media (min-width: 1280px) {
+    max-width: 1280px;
+  }
+`
