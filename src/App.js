26c5313c00963c9955caec6e55b3d83eb0c93c2c
wnpraf <rapribu@gmail.com>
2020-05-28 21:14:56 +0200

    Adjust app component to show all components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import React from 'react'
-import styled from 'styled-components'
+import { Hero } from '../components/HeroCard'
+import Container from '../components/Container'
+import { ReactComponent as Logo } from '../assets/logos/Logo_Sync_(Black).svg'
+import '../index.css'
+import LoginHeader from '../components/LoginHeader'
+import ContentWrapper from '../components/ContentWrapper'
+import PersonalBusiness from '../components/PersonalBusiness'
+import { SignUp } from '../components/SignUp'
+import { Footer } from '../components/Footer'
 
 function App() {
   return (
     <section className="app">
-      <h1>Hello Sync !!!</h1>
+      <Hero>
+        <Container>
+          <figure className="logo-wrapper">
+            <Logo />
+          </figure>
+          <ContentWrapper class="content-wrapper">
+            <LoginHeader />
+            <PersonalBusiness />
+          </ContentWrapper>
+          <SignUp />
+        </Container>
+      </Hero>
+      <Footer></Footer>
     </section>
   )
 }
